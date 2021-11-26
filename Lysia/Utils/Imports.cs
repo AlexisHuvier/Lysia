90e116e29bf048f322daa6abf3271d125c860d5d
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-11-26 01:38:02 +0100

    Add IO in Imports

--- Lysia/Utils/Imports.cs
+++ Lysia/Utils/Imports.cs
@@ -8,6 +8,13 @@ namespace Lysia.Utils
     {
         public static Dictionary<string, Dictionary<string, Type>> imports = new Dictionary<string, Dictionary<string, Type>>()
         {
+            { 
+                "IO", new Dictionary<string, Type>()
+                {
+                    { "display", typeof(IO.Display) },
+                    { "input", typeof(IO.Input) }
+                } 
+            }    
         };
 
         public static bool IsDefined(string name) => imports.ContainsKey(name);
