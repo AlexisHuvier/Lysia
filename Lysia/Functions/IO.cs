627e0ddd072a63275e37529869bfa10a41c7b9c2
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-11-26 01:37:31 +0100

    Create Input

--- Lysia/Functions/IO.cs
+++ Lysia/Functions/IO.cs
@@ -5,6 +5,19 @@ namespace Lysia.Functions
 {
     class IO
     {
+        public class Input
+        {
+            public static int[] nbParameters = new int[] { };
+            public static string[] typeParameters = new string[] { };
+            public static bool evaluateParameters = true;
+
+            public static dynamic Eval(Env env, List<dynamic> parameters)
+            {
+                Console.WriteLine(string.Join(" ", parameters));
+                return Console.ReadLine();
+            }
+        }
+
         public class Display
         {
             public static int[] nbParameters = new int[] { };
