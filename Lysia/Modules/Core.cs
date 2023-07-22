9c51a16bb400dac0bede09f84341fca9f22cc059
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-07-22 14:15:15 +0200

    feat(core): Add Cast, Def and Del

--- Lysia/Modules/Core.cs
+++ Lysia/Modules/Core.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Lysia.Core;
 using Lysia.Objects;
@@ -8,15 +9,100 @@ namespace Lysia.Modules;
 
 public class Core
 {
+    public class Cast : Function
+    {
+        public Cast(): base(new []{ 2 }, Array.Empty<string[]>(), false)
+        {}
+
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            
+            var value = Interpreter.Eval(values[1], env);
+            if (values[0] is Token.Token { Type: TokenType.Identifier } tok)
+            {
+                switch (tok.Value)
+                {
+                    case "int":
+                        if (int.TryParse(value, out int integer))
+                            return integer;
+                        Error.ShowError("WrongCast", "Cannot be int", values[1]);
+                        break;
+                    case "float":
+                        if (float.TryParse(value, out float single))
+                            return single;
+                        Error.ShowError("WrongCast", "Cannot be float", values[1]);
+                        break;
+                    case "string":
+                        return value.ToString();
+                    case "bool":
+                        if (bool.TryParse(value, out bool boolean))
+                            return boolean;
+                        Error.ShowError("WrongCast", "Cannot be boolean", values[1]);
+                        break;
+                    default:
+                        Error.ShowError("WrongCast", "Unknown Type", values[0]);
+                        break;
+                }
+            }
+
+            return null;
+        }
+    }
+    
     public class Comment : Function
     {
-        public Comment() : base(System.Array.Empty<int>(), System.Array.Empty<string[]>(), false)
+        public Comment() : base(Array.Empty<int>(), Array.Empty<string[]>(), false)
         {}
+    }
+
+    public class Def : Function
+    {
+        public Def(): base(new []{ 2 }, Array.Empty<string[]>(), false)
+        {}
+
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            var value = Interpreter.Eval(values[1], env);
+
+            if (values[0] is Token.Token { Type: TokenType.Identifier } tok)
+                env.Variables[tok.Value] = value;
+            else
+                Error.ShowError("WrongArgument", "Expected Identifier", values[0]);
+            return value;
+        }
+    }
+
+    public class Del : Function
+    {
+        public Del(): base(new []{ 1 }, Array.Empty<string[]>(), false)
+        {}
+
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            
+            if (values[0] is Token.Token { Type: TokenType.Identifier } token)
+            {
+                if (!env.IsDefine(token.Value))
+                    Error.ShowError("WrongArgument", "Identifier not defined", token);
+                else
+                {
+                    var value = env.Variables[token.Value];
+                    env.Variables.Remove(token.Value);
+                    return value;
+                }
+            }
+            else
+                Error.ShowError("WrongArgument", "Expected Identifier", values[0]);
+            return null;
+        }
     }
 
     public class Import : Function
     {
-        public Import() : base(new[] { 1 }, System.Array.Empty<string[]>(), false)
+        public Import() : base(new[] { 1 }, Array.Empty<string[]>(), false)
         { }
 
         public override dynamic? Eval(Env env, List<dynamic> parameters)
