5fd284106b7970d9cdb6c6fd39a3ab2ddf35c89f
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-07-23 00:21:36 +0200

    feat(example): try new core functions

--- LysiaExamples/Program.cs
+++ LysiaExamples/Program.cs
@@ -8,5 +8,15 @@ internal static class Program
         Console.WriteLine("BASIC EXAMPLE");
         Console.WriteLine("=============");
         Lysia.Program.RunFile(new[] { "Examples/basic" });
+        Console.WriteLine();
+        Console.WriteLine("=============");
+        Console.WriteLine("FOR EXAMPLE");
+        Console.WriteLine("=============");
+        Lysia.Program.RunFile(new[] { "Examples/for" });
+        Console.WriteLine();
+        Console.WriteLine("=============");
+        Console.WriteLine("FUNC EXAMPLE");
+        Console.WriteLine("=============");
+        Lysia.Program.RunFile(new[] { "Examples/func" });
     }
 }
