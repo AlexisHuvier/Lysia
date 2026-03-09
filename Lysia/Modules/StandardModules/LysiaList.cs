d021af8154bd8f6aa834cd12c4230ff836cb41ca
Alexis Huvier <lavapower84@gmail.com>
2026-03-09 22:49:45 +0100

    feat(module): finish implement of list

--- Lysia/Modules/StandardModules/LysiaList.cs
+++ Lysia/Modules/StandardModules/LysiaList.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Lysia.Core;
 using Lysia.Objects;
 using Lysia.Utils;
@@ -40,11 +41,145 @@ public class LysiaList
             return values[0][values[0].Count - 1];
         }
     }
+
+    [Docs("Range", "Creates a list of integers from start to end")]
+    [DocsExample("(list:range 1 5)", "[1, 2, 3, 4]")]
+    public class Range() : Function([2], [["int"], ["int"]], true)
+    {
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            var start = values[0];
+            var end = values[1];
+            return ((IEnumerable<int>)Enumerable.Range(start, end - start)).Select(x => (dynamic)x).ToList();
+        }
+    }
+
+    [Docs("Get", "Returns the element at the given index of a list")]
+    [DocsExample("(list:get [1 2 3] 1)", "2")]
+    public class Get() : Function([2], [["list"], ["int"]], true)
+    {
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            var list = values[0];
+            var index = values[1];
+            if (index >= 0 && index < list.Count) return list[index];
+            
+            Error.ShowError("Index out of range", parameters[0]);
+            return null;
+        }
+    }
+
+    [Docs("Len", "Returns the length of a list")]
+    [DocsExample("(list:len [1 2 3])", "3")]
+    public class Len() : Function([1], [["list"]], true)
+    {
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            var list = values[0];
+            return list.Count;
+        }
+    }
+
+    [Docs("Add", "Adds an element to the end of a list")]
+    [DocsExample("(list:add [1 2] 3)", "[1, 2, 3]")]
+    public class Add() : Function([2], [["list"], []], true)
+    {
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            values[0].Add(values[1]);
+            return values[0];
+        }
+    }
+
+    [Docs("Remove", "Removes an element from a list by index")]
+    [DocsExample("(list:remove [1 2 3] 1)", "[1, 3]")]
+    public class Remove() : Function([2], [["list"], ["int"]], true)
+    {
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            values[0].RemoveAt(values[1]);
+            return values[0];
+        }
+    }
+
+    [Docs("In", "Checks if an element is in a list")]
+    [DocsExample("(list:in [1 2 3] 2)", "true")]
+    public class In() : Function([2], [["list"], []], true)
+    {
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            return values[0].Contains(values[1]);
+        }
+    }
+
+    [Docs("Insert", "Inserts an element at a specific index in a list")]
+    [DocsExample("(list:insert [1 2 3] 1 4)", "[1, 4, 2, 3]")]
+    public class Insert() : Function([3], [["list"], ["int"], []], true)
+    {
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            values[0].Insert(values[1], values[2]);
+            return values[0];
+        }
+    }
+
+    [Docs("Replace", "Replaces an element at a specific index in a list")]
+    [DocsExample("(list:replace [1 2 3] 1 4)", "[1, 4, 3]")]
+    public class Set() : Function([3], [["list"], ["int"], []], true)
+    {
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            values[0][values[1]] = values[2];
+            return values[0];
+        }
+    }
+
+    [Docs("Clear", "Clears a list")]
+    [DocsExample("(list:clear [1 2 3])", "[]")]
+    public class Clear() : Function([1], [["list"]], true)
+    {
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            values[0].Clear();
+            return values[0];
+        }
+    }
+
+    [Docs("AddList", "Adds two lists together")]
+    [DocsExample("(list:+ [1 2] [3 4])", "[1, 2, 3, 4]")]
+    public class AddList() : Function([2], [["list"], ["list"]], true)
+    {
+        public override dynamic? Eval(Env env, List<dynamic> parameters)
+        {
+            var values = (List<dynamic>)base.Eval(env, parameters)!;
+            values[0].AddRange(values[1]);
+            return values[0];
+        }
+    }
     
     public static Dictionary<string, Function> GetImports() => new()
     {
         { "list:create", new Create() },
         { "list:first", new First() },
         { "list:last", new Last() },
+        { "list:range", new Range() },
+        { "list:get", new Get() },
+        { "list:len", new Len() },
+        { "list:add", new Add() },
+        { "list:remove", new Remove() },
+        { "list:in", new In() },
+        { "list:insert", new Insert() },
+        { "list:set", new Set() },
+        { "list:clear", new Clear() },
+        { "list:+", new AddList() }
     };
 }
--- docs/list.md
+++ docs/list.md
@@ -35,3 +35,113 @@ Parameters evaluated : Yes
 Examples : 
 - `(list:last [1 2 3])` => 3
 
+## Function - Range
+
+Creates a list of integers from start to end
+
+Symbol : list:range  
+Parameters : 2 - int, int  
+Parameters evaluated : Yes
+
+Examples : 
+- `(list:range 1 5)` => [1, 2, 3, 4]
+
+## Function - Get
+
+Returns the element at the given index of a list
+
+Symbol : list:get  
+Parameters : 2 - list, int  
+Parameters evaluated : Yes
+
+Examples : 
+- `(list:get [1 2 3] 1)` => 2
+
+## Function - Len
+
+Returns the length of a list
+
+Symbol : list:len  
+Parameters : 1 - list  
+Parameters evaluated : Yes
+
+Examples : 
+- `(list:len [1 2 3])` => 3
+
+## Function - Add
+
+Adds an element to the end of a list
+
+Symbol : list:add  
+Parameters : 2 - list,   
+Parameters evaluated : Yes
+
+Examples : 
+- `(list:add [1 2] 3)` => [1, 2, 3]
+
+## Function - Remove
+
+Removes an element from a list by index
+
+Symbol : list:remove  
+Parameters : 2 - list, int  
+Parameters evaluated : Yes
+
+Examples : 
+- `(list:remove [1 2 3] 1)` => [1, 3]
+
+## Function - In
+
+Checks if an element is in a list
+
+Symbol : list:in  
+Parameters : 2 - list,   
+Parameters evaluated : Yes
+
+Examples : 
+- `(list:in [1 2 3] 2)` => true
+
+## Function - Insert
+
+Inserts an element at a specific index in a list
+
+Symbol : list:insert  
+Parameters : 3 - list, int,   
+Parameters evaluated : Yes
+
+Examples : 
+- `(list:insert [1 2 3] 1 4)` => [1, 4, 2, 3]
+
+## Function - Replace
+
+Replaces an element at a specific index in a list
+
+Symbol : list:set  
+Parameters : 3 - list, int,   
+Parameters evaluated : Yes
+
+Examples : 
+- `(list:replace [1 2 3] 1 4)` => [1, 4, 3]
+
+## Function - Clear
+
+Clears a list
+
+Symbol : list:clear  
+Parameters : 1 - list  
+Parameters evaluated : Yes
+
+Examples : 
+- `(list:clear [1 2 3])` => []
+
+## Function - AddList
+
+Adds two lists together
+
+Symbol : list:+  
+Parameters : 2 - list, list  
+Parameters evaluated : Yes
+
+Examples : 
+- `(list:+ [1 2] [3 4])` => [1, 2, 3, 4]
+
