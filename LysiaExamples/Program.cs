641cdbcccfba64753dc42861f4f82701a3c9025f
Alexis Huvier <lavapower84@gmail.com>
2026-03-09 01:07:28 +0100

    feat(examples): rework with interactive run of examples

--- LysiaExamples/Program.cs
+++ LysiaExamples/Program.cs
@@ -1,29 +1,28 @@
-﻿Console.WriteLine("=============");
-Console.WriteLine("BASIC EXAMPLE");
-Console.WriteLine("=============");
-Lysia.Program.RunFile(["Examples/basic"]);
+﻿Console.WriteLine("Welcome to Lysia Examples!");
 Console.WriteLine();
-Console.WriteLine("=============");
-Console.WriteLine("CONDITION EXAMPLE");
-Console.WriteLine("=============");
-Lysia.Program.RunFile(["Examples/condition"]);
+Console.WriteLine($"List of current examples: {string.Join(", ", Directory.GetFiles("Examples").Select(Path.GetFileNameWithoutExtension))}");
 Console.WriteLine();
-Console.WriteLine("=============");
-Console.WriteLine("LOOP EXAMPLE");
-Console.WriteLine("=============");
-Lysia.Program.RunFile(["Examples/loop"]);
-Console.WriteLine();
-Console.WriteLine("=============");
-Console.WriteLine("FUNC EXAMPLE");
-Console.WriteLine("=============");
-Lysia.Program.RunFile(["Examples/func"]);
-Console.WriteLine();
-Console.WriteLine("=============");
-Console.WriteLine("FILE EXAMPLE");
-Console.WriteLine("=============");
-Lysia.Program.RunFile(["Examples/file"]);
-Console.WriteLine();
-Console.WriteLine("=============");
-Console.WriteLine("IMPORT EXAMPLE");
-Console.WriteLine("=============");
-Lysia.Program.RunFile(["Examples/import"]);
+while (true)
+{
+    Console.WriteLine("Which example do you want to run? (exit to quit - examples to list examples)");
+    var example = Console.ReadLine();
+    if (example == "exit")
+    {
+        Console.WriteLine("Exiting Lysia Examples...");
+        break;
+    }
+
+    if (example == "examples")
+    {
+        Console.WriteLine($"List of current examples: {string.Join(", ", Directory.GetFiles("Examples").Select(Path.GetFileNameWithoutExtension))}");
+        continue;
+    }
+    if (!File.Exists($"Examples/{example}.lysia"))
+    {
+        Console.WriteLine($"Unknown example : {example}");
+        continue;
+    }
+    
+    Lysia.Program.RunFile([$"Examples/{example}"]);
+    Console.WriteLine();
+}
